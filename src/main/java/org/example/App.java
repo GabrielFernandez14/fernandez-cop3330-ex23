138be45f3119ca8aff6b592feb0a0629c36a5990
GabrielFernandez14 <79731548+GabrielFernandez14@users.noreply.github.com>
2021-06-04 13:21:55 -0400

    Update App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,6 @@
-/* Gabriel Fernandez
- * COP3330 - Summer CV01
- * "Motivated" Practice Exercises
- * Exercise 23 - Troubleshooting Car Issues
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solutions
+ *  Copyright 2021 Gabriel Fernandez
  */
 
 package org.example;
